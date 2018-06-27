232495cd91495470709173240020f9665306b7c1
Katie Kwan <katiekwan@fb.com>
2018-06-27 11:06:55 -0700

    Initial Commit

---
+++ Flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flixster
+//
+//  Created by Katie Kwan on 6/27/18.
+//  Copyright © 2018 Katie Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flixster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Flixster
+//
+//  Created by Katie Kwan on 6/27/18.
+//  Copyright © 2018 Katie Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
