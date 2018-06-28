60cc6e4ffccf739816e85e18a0ff61f82b94d882
Katie Kwan <katiekwan@fb.com>
2018-06-28 15:13:46 -0700

    synopsis

---
+++ Flixster/View Controllers /DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Flixster
+//
+//  Created by Katie Kwan on 6/28/18.
+//  Copyright © 2018 Katie Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
